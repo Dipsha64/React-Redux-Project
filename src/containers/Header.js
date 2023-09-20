9006bbc48fb2c419ea18c9ed28871bc955c899ff
Dipsha Kalariya <dipshaj64@gmail.com>
2023-09-20 16:06:22 +0530

    Added header changes

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,7 +4,7 @@ const Header = () =>{
     return(
         <div className="ui fixed menu">
             <div className="ui container center">
-                <h2>JeDi's Shop</h2>
+                <h2>JD's Shop</h2>
             </div>
         </div>
     )
